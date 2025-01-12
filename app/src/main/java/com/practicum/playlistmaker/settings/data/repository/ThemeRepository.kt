67638a9cc874753da44ec73b847794af41393718
Kira <34008081+NoGameNoLifeProject@users.noreply.github.com>
2025-01-12 12:35:07 +0300

    default system dark theme

--- app/src/main/java/com/practicum/playlistmaker/settings/data/repository/ThemeRepository.kt
+++ app/src/main/java/com/practicum/playlistmaker/settings/data/repository/ThemeRepository.kt
@@ -1,12 +1,13 @@
 package com.practicum.playlistmaker.settings.data.repository
 
 import android.content.Context
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatDelegate
 import com.practicum.playlistmaker.settings.domain.models.ThemeSettings
 import com.practicum.playlistmaker.settings.domain.repository.IThemeRepository
 
 const val PREFERENCES_DARK_THEME = "dark_theme"
-class ThemeRepository(context: Context) : IThemeRepository {
+class ThemeRepository(val context: Context) : IThemeRepository {
     private val sharedPref = context.getSharedPreferences(PREFERENCES_DARK_THEME, Context.MODE_PRIVATE)
 
     override fun setTheme(theme: ThemeSettings) {
@@ -21,6 +22,11 @@ class ThemeRepository(context: Context) : IThemeRepository {
     }
 
     override fun getTheme(): ThemeSettings {
-        return ThemeSettings(sharedPref.getBoolean(PREFERENCES_DARK_THEME, false))
+        return ThemeSettings(sharedPref.getBoolean(PREFERENCES_DARK_THEME, isSystemInDarkTheme()))
+    }
+
+    private fun isSystemInDarkTheme(): Boolean {
+        val currentNightMode = context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
     }
 }
